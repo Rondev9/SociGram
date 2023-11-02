c115133575f1d3a1f45cf09b9871e5a096cf37ed
Rondev9 <ganesh8497993502@gmail.com>
2023-11-02 14:20:17 +0530

    integrated the auth functionalities, saving user to db and on sign up navigation to home, and worked on the user persistence using local storage

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
+  <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
         <App />
-    </BrowserRouter>
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>
 );
